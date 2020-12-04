ca15d60cd1d6da18c0c5ba18a2b8a53f32866816
Fly-fire-cpu <58647124+Fly-fire-cpu@users.noreply.github.com>
2020-12-04 10:59:20 +0800

    Update Hello.java

--- src/main/java/com/maven/Hello.java
+++ src/main/java/com/maven/Hello.java
@@ -3,6 +3,7 @@ package com.maven;
 public class Hello {
     public String sayHello(String name){
         System.out.println("尝试上传");
+        System.out.println("尝试下载");
         return "Hello "+name+"!";
 
     } }
